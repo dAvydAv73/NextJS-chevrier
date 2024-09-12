450ddcb124c45b7d6f2b7cd0506fe9c018849794
dAvydAv73 <davy.dauteuil@gmail.com>
2024-09-12 15:56:31 +0200

    Add unstable_setRequestLocale to page.js

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,31 +1,34 @@
+import { unstable_setRequestLocale } from 'next-intl/server';
 import { BlockRenderer } from "../../../components/BlockRenderer";
 import { getPage } from "../../../utils/getPage";
 import { notFound } from "next/navigation";
 import { getSeo } from "../../../utils/getSeo";
-import { useTranslations } from "next-intl";
 
-export default async function Home({ params }) {
-  const locale = params.locale || 'en';
+export default async function Home({ params: { locale } }) {
+  unstable_setRequestLocale(locale);
+
   const slug = locale === 'en' ? "/home" : "/";
   const data = await getPage(slug);
 
   if (!data) {
     notFound();
   }
- 
+
   return <BlockRenderer blocks={data} />;
 }
 
-export async function generateMetadata({ params }) {
-  const locale = params.locale || 'en';
+export async function generateMetadata({ params: { locale } }) {
+  unstable_setRequestLocale(locale);
+
   const slug = locale === 'en' ? "/home" : "/";
   const seo = await getSeo(slug);
+
   return {
     title: seo?.title || "",
     description: seo?.metaDesc || "",
   };
 }
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
   return [{ locale: 'en' }, { locale: 'fr' }];
 }
