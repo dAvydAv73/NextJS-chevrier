9331fd30f8cd59aaeeee62c5444ab7ab8c689781
dAvydAv73 <davy.dauteuil@gmail.com>
2024-09-24 17:49:32 +0200

    Configjs correigé pour le depoliement

--- next.config.js
+++ next.config.js
@@ -4,21 +4,56 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.js');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
-      protocol: 'http',
-      hostname: process.env.WP_IMAGES_URL,
-      port: '',
-      pathname: '/**' 
+        protocol: 'http',
+        hostname: process.env.WP_IMAGES_URL,
+        port: '',
+        pathname: '/**' 
       },
       {
-      protocol: 'https',
-      hostname: process.env.WP_IMAGES_URL,
-      port: '',
-      pathname: '/**' 
+        protocol: 'https',
+        hostname: process.env.WP_IMAGES_URL,
+        port: '',
+        pathname: '/**' 
       }
-   ],
+    ],
+  },
+  experimental: {
+    esmExternals: 'loose',
+    optimizeCss: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Optimisations webpack
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        commons: {
+          name: 'commons',
+          chunks: 'all',
+          minChunks: 2,
+        },
+      };
+    }
+
+    // Ajout du support pour le top-level await
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: '/_error',
+      },
+    ];
   },
 };
 
