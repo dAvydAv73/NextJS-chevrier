d612d47f6db5318d472244f20536b3595c0a4ab8
dAvydAv73 <davy.dauteuil@gmail.com>
2024-09-25 11:03:27 +0200

    Change font render on mobile

--- utils/fonts.js
+++ utils/fonts.js
@@ -10,12 +10,12 @@ export const getTextAlign = (textAlign = "left") => {
 
 export const getFontSizeForHeading = (level) => {
   const fontSizeMap = {
-    1: "text-6xl",
-    2: "text-5xl",
-    3: "text-4xl",
-    4: "text-3xl",
-    5: "text-2xl",
-    6: "text-xl",
+    1: "text-6xl sm:text-4xl",
+    2: "text-5xl sm:text-3xl",
+    3: "text-4xl sm:text-2xl",
+    4: "text-3xl sm:text-2xl",
+    5: "text-2xl sm:text-xl",
+    6: "text-xl  sm:text-xl",
   };
 
   return `${fontSizeMap[level] || ""}`;
